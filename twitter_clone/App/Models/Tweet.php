0d2223f125472702b2bcb3fae9c656a0df370855
kevinazevedo <taylorvision95@outlook.com>
2025-01-12 00:55:23 -0300

    Paginação de Registros

--- twitter_clone/App/Models/Tweet.php
+++ twitter_clone/App/Models/Tweet.php
@@ -53,6 +53,56 @@
  
             return $st->fetchAll(\PDO::FETCH_ASSOC);
         }
+
+        // recuperar por paginação
+        public function getPorPagina($limit, $offset) {
+            $query = "SELECT 
+                    t.id, t.id_usuario, u.nome, t.tweet, DATE_FORMAT(t.data, '%d/%m/%Y %H:%i') as data
+                FROM 
+                    tweets as t
+                LEFT JOIN 
+                    usuarios as u ON (t.id_usuario = u.id)
+                WHERE 
+                    t.id_usuario = :id_usuario
+                OR
+                    t.id_usuario in (SELECT id_usuario_seguindo FROM usuarios_seguidores WHERE id_usuario = :id_usuario)
+                ORDER BY
+                    t.data desc
+                LIMIT
+                    $limit
+                OFFSET
+                    $offset
+            ";
+
+            $st = $this->db->prepare($query);
+
+            $st->bindValue(":id_usuario", $this->__get("id_usuario"));
+            $st->execute();
+
+            return $st->fetchAll(\PDO::FETCH_ASSOC);
+        }
+
+        // recuperar total de tweets
+        public function getTotalRegistros() {
+            $query = "SELECT 
+                    COUNT(*) AS total
+                FROM 
+                    tweets as t
+                LEFT JOIN 
+                    usuarios as u ON (t.id_usuario = u.id)
+                WHERE 
+                    t.id_usuario = :id_usuario
+                OR
+                    t.id_usuario in (SELECT id_usuario_seguindo FROM usuarios_seguidores WHERE id_usuario = :id_usuario)
+            ";
+
+            $st = $this->db->prepare($query);
+
+            $st->bindValue(":id_usuario", $this->__get("id_usuario"));
+            $st->execute();
+
+            return $st->fetch(\PDO::FETCH_ASSOC);
+        }
     }
 
 ?>
